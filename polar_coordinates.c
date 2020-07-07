84d64f0db754d66b47b43331752f3f96624e50d0
Şeyda Arı <ariseydaari@gmail.com>
2020-07-08 00:45:24 +0300

    Update polar_coordinates.c

--- polar_coordinates.c
+++ polar_coordinates.c
@@ -9,7 +9,6 @@
 //- pprint prints a polar variable on console in form of (5.0/_30.0). 
 //- pscan prompts user to enter coordinates of polar point. 
 //- pmul computes multiplication of two polar and returns result 
-//- pdiv computes division of one polar to another.
 //Test your function in a program that receives two points in polar from user, computes multiplication and division, and prints result on console in polar form.   
 
 struct polar{
